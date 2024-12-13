b6cbbc8c4707501cd702e7c1069687ee4a3b0511
shyamabhishek <shyamabhishekkumar@gmail.com>
2024-12-14 00:16:44 +0530

    updated 13dec

---
+++ ConsoleApp1/MethodHidingDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MethodHidingDemo
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Parent Show method called");
+
+        }
+        public void Test()
+        {
+            Console.WriteLine("Parent Test Method called ");
+        }
+    }
+    class MethodHidingChildDemo : MethodHidingDemo
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Child Show Method called");
+        }
+        public new void Test()
+        {
+            Console.WriteLine("Child Test method show");
+        }
+    
+        static void Main()
+        {
+            MethodHidingChildDemo obj = new MethodHidingChildDemo();
+            //MethodHidingDemo obj1 = new MethodHidingDemo();
+            //obj1.Show();
+            //obj1.Test();
+            obj.Show();
+            obj.Test();
+            Console.ReadLine();
+        }
+    }
+}
