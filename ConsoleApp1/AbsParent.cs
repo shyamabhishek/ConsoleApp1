631889e88d96b07954426bd34b581b3dcdf52806
shyamabhishek <shyamabhishekkumar@gmail.com>
2024-12-15 18:53:21 +0530

    updated 15dec

---
+++ ConsoleApp1/AbsParent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal abstract class AbsParent
+    {
+        public int I { get; set; }
+        public string S {  get; set; }
+        public bool B { get; set; }
+        public void Add(int x, int y) //non abstract method
+        {
+            Console.WriteLine(x + y);
+        }
+        public void Sub(int x, int y)//non abstract method
+        {
+            Console.WriteLine(x - y);
+        }
+        public abstract void Mul(int x, int y); // abstract method
+
+        public abstract void Div(int x, int y); //abstract method
+    }
+    class AbsChild : AbsParent
+    {
+        public override void Div(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+
+        public override void Mul(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+        static void Main ()
+        {
+            AbsChild obj = new AbsChild();
+            AbsParent absParent = obj; //refference
+            absParent.Add(1, 2);
+            absParent.Sub(20, 10);
+            absParent.Mul(20, 10);
+            absParent.Div(20, 10);
+            absParent.I = 10;
+            absParent.S = "Testing";
+            absParent.B = true;
+            Console.WriteLine(absParent.I);
+            Console.WriteLine(absParent.S);
+            Console.WriteLine(absParent.B);
+            Console.ReadLine ();
+        }
+    }
+}
---
+++ ConsoleApp1/CommonFigure.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class CommonFigure
+    {
+        public double width, height, radius;
+        public const float Pi = 3.14f;
+        public abstract double GetArea();
+    }
+    public class Rectangle : CommonFigure 
+    {
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double GetArea()
+        {
+            return width * height;
+        }
+    }
+    public class Circle : CommonFigure
+    {
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()
+        {
+           return Pi * radius*radius;
+        }
+    }
+    public class Cone : CommonFigure
+    {
+        public Cone(double radius, double height)
+        {
+            this.radius = radius;
+            this.height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Pi * radius * radius + Pi * radius* height ;
+        }
+    }
+    public class Triangle : CommonFigure
+    {
+        public Triangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double GetArea()
+        {
+            return 0.5 *width * height;
+        }
+    }
+    public class TestClass
+    {
+        static void Main()
+        {
+            Rectangle rt = new Rectangle(30.33, 23.44);
+            Console.WriteLine("Area of Rectangle" + rt.GetArea());
+            Circle cs = new Circle(5.7);
+            Console.WriteLine("Area of Circle" + cs.GetArea());
+            Triangle tr = new Triangle(7.8, 8.1);
+            Console.WriteLine("Area of Triangle" + tr.GetArea());
+            Cone cn = new Cone(7.3, 9.4);
+            Console.WriteLine("Area of Cone" + cn.GetArea());
+            Console.ReadLine();
+
+        }
+    }
+}
