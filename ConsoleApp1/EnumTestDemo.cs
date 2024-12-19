af0c3bbe17a77cdb848657ac3274535bd799c1c8
shyamabhishek <shyamabhishekkumar@gmail.com>
2024-12-19 23:40:10 +0530

    updated 19th dec

---
+++ ConsoleApp1/EnumTestDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum Days : byte
+    {
+        Monday = 1,
+        Tuesday = 4,
+        Wednesday = 6,
+        Thursday = 10,
+        Friday = 12
+    }
+
+    internal class EnumTestDemo
+    {
+        public static Days MeetingDate { get; set; } = Days.Monday;
+        static void Main()
+        {
+            //Console.BackgroundColor = ConsoleColor.Red;
+            //Console.WriteLine("Welcome to Dotnet");
+            //Console.ReadLine();
+            //Days d = Days.Monday;
+            //Days d = 0;
+            //Console.WriteLine(d);
+            //foreach(byte i in Enum.GetValues(typeof(Days)))
+            //{
+            //    Console.WriteLine(i +":" +(Days)i);
+            //}
+            Console.WriteLine(MeetingDate); //static block can always directly called into static
+            MeetingDate = Days.Wednesday; //reschdule meeting on wednesday instead of thursday
+            Console.ReadLine();
+        }
+    }
+}
