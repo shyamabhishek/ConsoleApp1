86c6aa0af7f5030ab35f89c22160b7ff3581003a
shyamabhishek <shyamabhishekkumar@gmail.com>
2024-12-20 21:59:36 +0530

    updated 20th dec

---
+++ ConsoleApp1/StaticDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal static class StaticDemo
+    {
+        public static string Name;
+        static StaticDemo()
+        {
+            Console.WriteLine("Static Constructor Called");
+        }
+        public static void Test()
+        {
+            Console.WriteLine("Test Method Called");
+        }
+        //public void Test2()
+        //{
+        //    Console.WriteLine("Test 2 Method Called");
+        //}
+        static void Main()
+        {
+            StaticDemo.Test();
+            StaticDemo.Name = "Testing Name";
+            Console.ReadLine();
+        }
+    }
+}
