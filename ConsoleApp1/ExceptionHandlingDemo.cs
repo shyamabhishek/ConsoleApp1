ae6f06b99dfc5f809fd6de1f1c11b1b908d9dd24
shyamabhishek <shyamabhishekkumar@gmail.com>
2025-01-04 19:32:58 +0530

    updated 4th january 2025

---
+++ ConsoleApp1/ExceptionHandlingDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ExceptionHandlingDemo
+    {
+        public static void TestException()
+        {
+            try
+            {
+                int x = 10;
+                int res = x / 0;
+                Console.WriteLine(res);
+            }
+            catch (DivideByZeroException d)
+            {
+                Console.WriteLine("Message " + d.Message);
+                Console.WriteLine("============================================");
+                Console.WriteLine("StackTree " + d.StackTrace);
+                Console.WriteLine("============================================");
+                Console.WriteLine("InnerException " + d.InnerException);
+                Console.WriteLine("============================================");
+                Console.ReadLine();
+            }
+            
+        }
+        static void Main()
+        {
+            TestException();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/RefOutDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class RefOutDemo
+    {
+        //using ref keyword
+        //static void Main()
+        //{
+        //    int x = 20;
+        //    TestFunction(ref x);
+        //    Console.WriteLine(x);
+        //    Console.ReadLine();
+        //}
+
+        //static void TestFunction(ref int Value)
+        //{
+        //    Value = Value + 10;
+        //}
+
+
+        //out Keyword
+        static void Main()
+        {
+            int x = 20;
+            TestFunction(out x);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+
+        static void TestFunction(out int Value)
+        {
+            Value = 0;
+            Value = Value + 10;
+        }
+    }
+}
